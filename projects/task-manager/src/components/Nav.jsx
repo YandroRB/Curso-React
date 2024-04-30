a0b3b8e60b8705259836191efbc659559e8e6cea
YandroRB <santiago.rdbn@outlook.com>
2024-04-30 09:43:21 -0500

    Agregacion de efecto hover en la navegacion de la vista del task

--- projects/task-manager/src/components/Nav.jsx
+++ projects/task-manager/src/components/Nav.jsx
@@ -6,13 +6,13 @@ function Nav() {
     <>
       <nav className=" p-4 space-x-4 flex ">
         <NavLink
-          className={({ isActive }) => (isActive ? "font-semibold flex items-center border-b-4 border-indigo-500 pr-2 " : "flex items-center text-black/50")}
+          className={({ isActive }) => (isActive ? "font-semibold flex items-center border-b-2 border-indigo-500  hover:bg-indigo-300 py-1 px-2 rounded-t-md" : "flex items-center text-black/50 hover:bg-indigo-300 py-1 px-2 rounded-md")}
           to={"/tasks/board"}
         >
          <IconClipboard2Fill className=" h-5 w-5 mr-1"/> Tablero 
         </NavLink>
         <NavLink
-          className={({ isActive }) => (isActive ? "font-semibold flex items-center border-b-4 border-indigo-500 pr-2" : " flex items-center text-black/50")}
+          className={({ isActive }) => (isActive ? "font-semibold flex items-center border-b-2 border-indigo-500 hover:bg-indigo-300 py-1 px-2 rounded-t-md " : " flex items-center text-black/50 hover:bg-indigo-300 py-1 px-2 rounded-md")}
           to={"/tasks/list"}
         >
          <IconTableList className="h-5 w-5 mr-1"/> Lista
