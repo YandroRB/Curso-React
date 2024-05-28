b02aaf66d8a29c023236949238b59dd9f9fc76dd
YandroRB <santiago.rdbn@outlook.com>
2024-05-28 18:28:35 -0500

    Funcionamiento del formulario de contacto

--- projects/PersonalBlog/src/components/FormContact.tsx
+++ projects/PersonalBlog/src/components/FormContact.tsx
@@ -1,33 +1,70 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import credentials from '../credentials.json';
+
 function FormContact() {
+  const refForm = useRef<HTMLFormElement>(null);
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (!refForm.current) return;
+    emailjs
+      .sendForm(
+        credentials.serviceID,
+        credentials.emailTemplateID,
+        refForm.current,
+        {
+          publicKey: credentials.publicKey,
+          limitRate: {
+            throttle: 300000,
+          },
+        }
+      )
+      .then(() => {
+        console.log('ok');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
-    <form className="flex flex-col max-w-[420px] gap-y-1 mx-auto">
+    <form
+      ref={refForm}
+      onSubmit={handleSubmit}
+      className="flex flex-col max-w-[420px] gap-y-1 mx-auto"
+    >
       <label className=" text-gray-100" htmlFor="nombre">
         Nombre
       </label>
       <input
         className=" rounded-lg px-3 py-1 bg-gray-500"
         autoComplete="off"
+        name="from_name"
         type="text"
         id="nombre"
         placeholder="¿Cual es tu nombre?"
+        required
       />
       <label className=" text-gray-100" htmlFor="correo">
         Correo Electronico
       </label>
       <input
         className=" rounded-lg px-3 py-1 bg-gray-500"
-        type="text"
+        name="from_email"
+        type="email"
         id="correo"
         placeholder="Tu correo electronico"
+        required
       />
       <label className=" text-gray-100" htmlFor="comentario">
         Nombre
       </label>
       <textarea
         className=" rounded-lg px-3 py-1 bg-gray-500 resize-none"
+        name="message"
         id="comentario"
         placeholder="¿Sobre qué quieres hablarme?"
         rows={7}
+        required
       ></textarea>
       <button
         type="submit"
